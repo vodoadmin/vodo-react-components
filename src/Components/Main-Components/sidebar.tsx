6a977c9cc3cd9302f4578b1bfae1949adc590dab
Mustafa Ahmed <mostafafikry97@gmail.com>
2024-07-20 23:28:41 +0300

    sidebar modified

--- src/Components/Main-Components/sidebar.tsx
+++ src/Components/Main-Components/sidebar.tsx
@@ -57,25 +57,30 @@ const SideBar = ({
     >
       <div className="flex w-full items-center justify-end gap-[70px]">
         {open && <LogoIcon />}
-        <Button className="w-16 py-8 rounded-none " onClick={() => handleSideBar()}>
+        <Button
+          className="w-16 py-8 rounded-none "
+          onClick={() => handleSideBar()}
+        >
           {open ? <ArrowMenuIcon stroke="white" /> : <BurgerIcon />}
         </Button>
       </div>
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1" className="w-full border-none">
-          <AccordionTrigger
-            onClick={(e: any) =>
-              e.currentTarget.classList.toggle("vodo-rc-sidebar-active")
-            }
-            className="hover:no-underline w-full p-0 rounded-none  py-6 !flex !justify-start px-5 gap-4 vodo-rc-icon-trigger after:rounded-none"
-          >
-            <ModulesIcon />
-            {open && <span className=" text-black/50">Modules</span>}
-          </AccordionTrigger>
+          {subModules?.length && (
+            <AccordionTrigger
+              onClick={(e: any) =>
+                e.currentTarget.classList.toggle("vodo-rc-sidebar-active")
+              }
+              className="hover:no-underline w-full p-0 rounded-none  py-6 !flex !justify-start px-5 gap-4 vodo-rc-icon-trigger after:rounded-none"
+            >
+              <ModulesIcon />
+              {open && <span className=" text-black/50">Modules</span>}
+            </AccordionTrigger>
+          )}
           <AccordionContent className="p-0 pl-11">
             {open && (
               <ul className="*:my-3">
-                {subModules &&
+                {subModules?.length &&
                   subModules.map((module: SubModuleProps, i) => (
                     <SideModuleCard
                       key={i}
@@ -92,7 +97,7 @@ const SideBar = ({
       </Accordion>
 
       <div className="flex flex-col w-full gap-1">
-        {modules &&
+        {modules?.length &&
           modules.map((module: ModuleProps, i) => (
             <SideCard
               key={i}

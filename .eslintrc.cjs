293dfca3e4b922cc4805c1fc2605f8786018b463
Mustafa Ahmed <mostafafikry97@gmail.com>
2024-08-05 20:48:55 +0300

    eslint updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,12 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh", "@typescript-eslint"],
   rules: {
-    "react-refresh/only-export-components": "error",
+    "react-refresh/only-export-components": "off",
     // other rules
+  },
+  settings: {
+    react: {
+      version: "detect", // Detect the version of React
+    },
   },
 };
